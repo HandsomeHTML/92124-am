6e9ac80c53c0235ce5e74357c35ee6eb4826b175
HandsomeHTML <106398798+HandsomeHTML@users.noreply.github.com>
2024-09-21 11:21:47 -0500

    Add files via upload

---
+++ my tree script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo
+if [ "$1" != "" ]
+  then cd "$1"
+fi
+pwd
+if [ `ls -F -1 | grep "/" | wc -l` = 0 ]
+  then echo " -> no sub-directories"
+fi
+ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
+echo
+./
